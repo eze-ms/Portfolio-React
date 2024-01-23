ed9aafdc3c500a66bdea121c342670de3a2db960
eze110011 <ezequielmacchi@MacBook-Pro-de-Ezequiel-2.local>
2024-01-23 20:21:05 +0100

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,31 @@
-import React, { useState } from 'react';
-// import { Link, Router, Routes } from 'react-router-dom';
-import { Contexto } from './componentes/Contexto';  
-import Header from './componentes/Header';
-import Resume from './componentes/Resume';
-import About from './componentes/About';
+import React from 'react';
+import NavBar from './componentes/NavBar';
+import Home from './pages/home/Home';
+import About from './pages/about/About';
+import Portfolio from './pages/portfolio/Portfolio';
+import Contacto from './pages/contact/Contact';
+ 
+import { Route, Routes } from 'react-router-dom';
+
+
 
 
 import './App.css';
 
 function App() {
 
-  const [temaOscuro, cambiarTema] = useState(false);
 
   return (
     <div className="App">
-      <Contexto.Provider
-        value={
-          {
-            temaOscuro,
-            cambiarTema,
-          }
-        }
-      >
-        <Header />
-        <Resume />
-        <About />
+      <NavBar /> 
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="portfolio" element={<Portfolio />} />
+        <Route path="contacto" element={<Contacto />} />
+      </Routes>
 
-      </Contexto.Provider>
+     
     </div>
   );
 }
---
+++ src/componentes/NavBar.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+import { NavLink } from 'react-router-dom';
+import { links } from '../data';
+import "./NavBar.css"
+
+
+const NavBar = () => {
+    const [showMenu, SetShowMenu] = useState(false);
+
+    return (
+        <nav className='nav'>
+            <div className={`${showMenu ? 'nav__menu show-menu' : 'nav__menu'}`}>
+                <ul className="nav__list">
+                    {links.map(({ name, icon, path }, index)=>{
+                        return (
+                            <li className='nav__item' key={index}>
+                                <NavLink to={path} className={({isActive}) =>
+                                    isActive? 'nav__link active-nav' : 'nav__link'
+                                    }
+                                >
+                                    {icon}
+                                    <h3 className='nav__name'>{name}</h3>
+                                </NavLink>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </div>
+            
+            <div 
+                className={`${showMenu ? 'nav__togge animate-toggle' : 'nav__toggle'}`}
+                onClick={() => SetShowMenu(!showMenu)}
+            >
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
