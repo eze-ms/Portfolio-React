58abecae1afeee6010eae989a37fce3ddce7658c
eze110011 <ezequielmacchi@MacBook-Pro-de-Ezequiel-2.local>
2024-01-24 12:52:29 +0100

    cambios css

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      About
-    </div>
+    <main className='section container'>
+      <section className="about">
+        <h2 className="section__title">Acerca <span>de mí</span></h2>
+      </section>
+    </main> 
   )
 }
 
