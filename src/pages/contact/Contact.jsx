5eb07df60dff2678a96d5a893408304adf65b94b
eze110011 <ezequielmacchi@MacBook-Pro-de-Ezequiel-2.local>
2024-01-29 21:06:51 +0100

    cambios en css

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,10 +1,101 @@
 import React from 'react'
+import{
+  FaEnvelopeOpen,
+  FaPhoneSquareAlt,
+  FaLinkedin,
+  FaGithub
+} from 'react-icons/fa';
+
+import{
+  FiSend
+}from 'react-icons/fi'
+
+import "./Contact.css";
+
+
 
 const Contact = () => {
   return (
-    <div>
-      Contacto
-    </div>
+    <section className="contact section">
+      <h2 className="section__title"><span>Contacto</span></h2>
+
+      <div className="contact__container container grid">
+        <div className="contact__data">
+          <h3 className="contact__title">No seas tímido!</h3>
+          <p className="contact__description">
+            No dudes en ponerte en contacto conmigo. Siempre estoy abierto a discutir sobre nuevas propuestas, ideas creativas u oportunidades para aportar mejoras a los proyectos.
+          </p>
+          <div className="contact__info">
+            <div className="info__item">
+              <FaEnvelopeOpen className='info__icon' />
+
+              <div>
+                <span className="info__title">Escríbeme</span>
+                <h4 className="info__desc">macchiezequiel@gmail.com</h4>
+              </div>
+               
+            </div>
+
+            <div className="info__item">
+              <FaPhoneSquareAlt className='info__icon' />
+
+              <div>
+                <span className="info__title">Llámame</span>
+                <h4 className="info__desc">691569610</h4>
+              </div>
+             
+            </div>
+          </div>  
+          <div className="contact__socials">
+            <a href="https://www.linkedin.com/in/ezems/" className="contact__socials-link">
+              <FaLinkedin/>
+            </a>
+            <a href="https://github.com/eze-ms/" className="contact__socials-link">
+              <FaGithub/>
+            </a>
+          </div>
+        </div>
+        
+        <form className="contact__form">
+            <div className="form__input-group">
+              <div className="form__input-div">
+                <input 
+                  type="text" 
+                  placeholder='Nombre' 
+                  className='form__control'/>
+              </div>
+              <div className="form__input-div">
+                <input 
+                  type="email" 
+                  placeholder='Email' 
+                  className='form__control'/>
+              </div>
+              <div className="form__input-div">
+                <input 
+                  type="text" 
+                  placeholder='Asunto' 
+                  className='form__control'/>
+              </div>
+            </div>
+
+            <div className="form__input-div-txt">
+                <textarea 
+                  type="text" 
+                  placeholder="Mensaje" 
+                  className="form__control textarea">
+                </textarea>
+            </div>
+
+            <button className='button'>
+              Enviar mensaje
+              <span className="button__icon contact__button-icon">
+                  <FiSend/>
+                </span>
+            </button>
+               
+        </form>
+      </div>
+    </section>
   )
 }
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,11 @@ import Profile from '../../assets/home.jpg'
 import { Link } from 'react-router-dom';
 import { FaArrowRight } from "react-icons/fa";
 import "./Home.css"
+import{
+ 
+  FaLinkedin,
+  FaGithub
+} from 'react-icons/fa';
 
 const Home = () => {
   return (
@@ -20,11 +25,23 @@ const Home = () => {
             <span>Desarrollador Front-end</span>
           </h2>
           <p className="home__description">Mi enfoque se centra en la creación de un código limpio, funcional e impactante para garantizar experiencias visuales atractivas en cada proyecto.</p>
-
+          <div className='box__content-button-socials'>
           <Link to='/about' className='button'>Más acerca de mí <span className="button__icon"><FaArrowRight /></span>
           </Link>
+          <div className="contact__socials-home">
+            <a href="https://www.linkedin.com/in/ezems/" className="contact__socials-link">
+              <FaLinkedin/>
+            </a>
+            <a href="https://github.com/eze-ms/" className="contact__socials-link">
+              <FaGithub/>
+            </a>
+          </div>
+          </div>
+          
         </div>
       </div>
+
+      
 
       <div className="color__block"></div>
       </section>
